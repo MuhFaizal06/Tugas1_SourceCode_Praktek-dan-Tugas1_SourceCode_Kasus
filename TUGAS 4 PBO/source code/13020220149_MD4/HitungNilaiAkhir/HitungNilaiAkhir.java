f40050defcd90c99ccb8984803d50a4a7e7f63cf
MuhFaizal06 <160796094+MuhFaizal06@users.noreply.github.com>
2024-03-27 10:58:14 +0800

    Add files via upload

---
+++ TUGAS 4 PBO/source code/13020220149_MD4/HitungNilaiAkhir/HitungNilaiAkhir.java
@@ -0,0 +1,12 @@
+// 13020220149, Muh. Faizal, Sabtu 24 Maret 2024
+package evaluasi.HitungNilaiAkhir;
+
+public class HitungNilaiAkhir {
+    public double NilaiTugas(int tugas1, int tugas2) {
+        return (tugas1 + tugas2) * 2.0;
+    }
+
+    public double nilaiAkhir(double tugas, int mid, int uas) {
+        return (tugas * 0.4) + (mid * 0.3) + (uas * 0.3);
+    }
+}
---
+++ TUGAS 4 PBO/source code/13020220149_MD4/Mahasiswa/Identitas.java
@@ -0,0 +1,22 @@
+// 13020220149, Muh. Faizal, Sabtu 24 Maret 2024
+package evaluasi.mahasiswa;
+
+public class Identitas {
+    private String nama;
+    private String stambuk;
+    
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public void setStambuk(String stambuk){
+        this.stambuk = stambuk;
+    }
+    public String getStambuk(){
+        return stambuk;
+    }
+
+    
+}
